59b5a06041c029f5ca54647c226521ea8567fa01
Timdo45 <70977334+Timdo45@users.noreply.github.com>
2021-10-17 19:30:42 -0400

    Add files via upload

--- receive.py
+++ receive.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import pika
 
+credentials = pika.PlainCredentials('admin', 'admin')
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
 channel.basic_consume(queue='hello',
                       auto_ack=True,
                       on_message_callback=callback)
@@ -21,4 +24,5 @@ if __name__ == '__main__':
             sys.exit(0)
         except SystemExit:
             os._exit(0)
+
 
--- send.py
+++ send.py
@@ -1,14 +1,15 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+credentials=pika.PlainCredentials('admin','admin')
+connection = pika.BlockingConnection(pika.ConnectionParameters('serverb.example.com',5672,'/',credentials))
 channel = connection.channel()
-
+val=input("PLease put message to send ")
 channel.queue_declare(queue='hello')
-
 channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+                      body=val)
+print(" [x] Sent " + val)
 
 connection.close()
+
 
