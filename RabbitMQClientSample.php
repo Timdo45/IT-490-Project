e0c74f30fe4821c69d9e7fc3b27432d34adea939
matin4real <89930224+matin4real@users.noreply.github.com>
2021-11-01 22:08:18 -0400

    Add files via upload

--- RabbitMQClientSample.php
+++ RabbitMQClientSample.php
@@ -1,21 +1,46 @@
 <?php
+require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
-if(isset($argv[1])){
-	$msg = $argv[1];
+function registerMessage($Username, $Password){
+	$client = new RabbitMQClient('databaseRabbitMQ.ini', 'it490Server');
+	if(isset($argv[1])){
+	}
+	else{
+		$msg = array("message"=>"Register", "type"=>"register", "username" => $Username, "password" => $Password );
+
+	}
+
+
+
+	$response = $client->send_request($msg);
+
+	//echo "client received response: " . PHP_EOL;
+	return($response);
+	//echo "\n\n";
+
+	if(isset($argv[0]))
+		echo $argv[0] . " END".PHP_EOL;
 }
-else{
-	$msg = array("message"=>"Register", "type"=>"register", "username"=>"test", "password"=>"1234");
+
+function loginMessage($Username, $Password){
+
+	$client = new RabbitMQClient('databaseRabbitMQ.ini', 'it490Server');
+	if(isset($argv[1])){
+		$msg = $argv[1];
+	}
+	else{
+		$msg = array("message"=>"Login", "type"=>"login", "username" => $Username, "password" => $Password);
+		//server listens for "login" in processor function then points to login function
+	}
+
+	$response = $client->send_request($msg);
+
+	//echo "client received response: " . PHP_EOL;
+	return($response);
+	//echo "\n\n";
+
+	if(isset($argv[0]))
+		echo $argv[0] . " END".PHP_EOL;
 }
-
-$response = $client->send_request($msg);
-
-echo "client received response: " . PHP_EOL;
-print_r($response);
-echo "\n\n";
-
-if(isset($argv[0]))
-	echo $argv[0] . " END".PHP_EOL;
-?>
